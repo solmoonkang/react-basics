c3b222881276bc7dfbea284ba238bdd3ca72ca12
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-29 15:50:36 +0900

    feat: Implement using map method for todo list in App.js

--- src/App.js
+++ src/App.js
@@ -35,6 +35,19 @@ export default class App extends Component {
       textDecoration: "none",
     };
   };
+
+  todoData = [
+    {
+      id: "1",
+      title: "공부하기",
+      completed: true
+    },
+    {
+      id: "2",
+      title: "청소하기",
+      completed: false
+    }
+  ];
   
   render() {
     return (
@@ -44,11 +57,13 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          <div style={this.getStyle()}>
+          {this.todoData.map(data => (
+            <div style={this.getStyle()} key={data.id}>
             <input type="checkbox" defaultChecked={false} />
-            공부하기
+            {data.title}
             <button style={this.btnStyle}>x</button>
           </div>
+          ))}
         </div>
       </div>
     );
