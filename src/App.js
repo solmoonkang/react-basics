5c16f8d6807da807f8b64c3fa6e67933a9669e10
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-29 17:11:59 +0900

    feat: Implement draw a line depending on whether it's completed or not in App.js

--- src/App.js
+++ src/App.js
@@ -20,18 +20,7 @@ import './App.css';
 export default class App extends Component {
 
   state = {
-    todoData: [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false
-      },
-    ],
+    todoData: [],
     value: "",
   };
 
@@ -44,11 +33,11 @@ export default class App extends Component {
     float: "right",
   };
 
-  getStyle = () => { 
+  getStyle = (completed) => { 
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none",
+      textDecoration: completed ? "line-through" : "none",
     };
   };
 
@@ -72,8 +61,19 @@ export default class App extends Component {
     };
 
     // 원래 있던 할 일에 새로운 할 일 더해주기
-    this.setState({ todoData: [...this.state.todoData, newTodo] });
+    this.setState({ todoData: [...this.state.todoData, newTodo], value: "" });
   };
+
+  handleCompleChange = (id) => {
+    let newTodoData = this.state.todoData.map((data) => {
+      if (data.id === id) {
+        data.completed =!data.completed;
+      }
+      return data;
+    });
+    
+    this.setState({ todoData: newTodoData });
+  }
   
   render() {
     return (
@@ -84,8 +84,8 @@ export default class App extends Component {
           </div>
 
           {this.state.todoData.map(data => (
-            <div style={this.getStyle()} key={data.id}>
-            <input type="checkbox" defaultChecked={false} />
+            <div style={this.getStyle(data.completed)} key={data.id}>
+            <input type="checkbox" onChange={() => this.handleCompleChange(data.id)} defaultChecked={false} />
             {" "}{data.title}
             <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>x</button>
           </div>
