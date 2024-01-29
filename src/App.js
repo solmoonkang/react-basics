22ea82f90d4fb195d64c6787f3d6047d5f093d8c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-29 15:42:41 +0900

    feat: Implement list styling in App.js

--- src/App.js
+++ src/App.js
@@ -28,6 +28,14 @@ export default class App extends Component {
     float: "right",
   };
 
+  getStyle = () => { 
+    return {
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: "none",
+    };
+  };
+  
   render() {
     return (
       <div className="container">
@@ -36,7 +44,7 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          <div>
+          <div style={this.getStyle()}>
             <input type="checkbox" defaultChecked={false} />
             공부하기
             <button style={this.btnStyle}>x</button>
