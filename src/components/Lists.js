1f28aa052fb85849c16837a0d3c37f5556dc09b9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 17:42:47 +0900

    feat: Implement saving in localStroage upon refresh in Lists.js file

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -17,6 +17,7 @@ const Lists = React.memo(({ todoData, setTodoData, handleClick }) => {
         // 원하는 자리에 reorderItem을 insert 해준다.
         newTodoData.splice(result.destination.index, 0, reorderedItem);
         setTodoData(newTodoData);
+        localStorage.setItem("todoData", JSON.stringify(newTodoData));
     }
 
     return (
