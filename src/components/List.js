0bcba48201febd54d4a9ff6195eebe096bafecaf
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 15:14:57 +0900

    feat: Implement List component import and props part in List.js

--- src/components/List.js
+++ src/components/List.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function List() {
+function List({ id, title, completed, todoData, setTodoData, provided, snapshot }) {
 
     const handleClick = (id) => {
         let newTodoData = todoData.filter((data) => data.id!== id);
@@ -20,13 +20,13 @@ function List() {
     return (
         <div 
         className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounderd`} 
-        key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
+        key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
             <div className="items-center">
-                <input type="checkbox" onChange={() => handleCompleChange(data.id)} defaultChecked={false} />{" "}
-                <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
+                <input type="checkbox" onChange={() => handleCompleChange(id)} defaultChecked={false} />{" "}
+                <span className={completed ? "line-through" : undefined}>{title}</span>
             </div>
             <div className="items-center">
-                <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
+                <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x</button>
             </div>
         </div>
       )
