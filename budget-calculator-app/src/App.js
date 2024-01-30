027ef5238dbee172741f649b17196ae679928c48
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 18:29:36 +0900

    Add files budget-calculator-app

--- budget-calculator-app/src/App.js
+++ budget-calculator-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-yellow-100">
-      <div>
+      <div className>
         <h1>예산 계산기</h1>
       </div>
     </div>
