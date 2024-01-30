6f8d2a80ced2ed9420692bdba7f89cca06bda2e1
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 15:04:27 +0900

    feat: Implement List component divide in Lists.js

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -41,17 +41,7 @@ export default function List({todoData, setTodoData}) {
                             <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
                             
                                 {(provided, snapshot) => (
-                                    <div 
-                                    className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounderd`} 
-                                    key={data.id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
-                                        <div className="items-center">
-                                            <input type="checkbox" onChange={() => handleCompleChange(data.id)} defaultChecked={false} />{" "}
-                                            <span className={data.completed ? "line-through" : undefined}>{data.title}</span>
-                                        </div>
-                                        <div className="items-center">
-                                            <button className="px-4 py-2 float-right" onClick={() => handleClick(data.id)}>x</button>
-                                        </div>
-                                    </div>
+                                    
                                 )}
                             </Draggable>
                             ))}
