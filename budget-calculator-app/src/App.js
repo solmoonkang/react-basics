830d704c704b92324888732f3eb3630ca7c1539c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 18:28:41 +0900

    Move files to todo-app

---
+++ budget-calculator-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-yellow-100">
+      <div>
+        <h1>예산 계산기</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
