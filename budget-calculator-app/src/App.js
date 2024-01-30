8349b1ea07459991860c945d754b74f9c78d4cf9
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 20:25:18 +0900

    feat: Implement basic structure for budget calculator app

--- budget-calculator-app/src/App.js
+++ budget-calculator-app/src/App.js
@@ -3,8 +3,23 @@ import './App.css';
 function App() {
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-yellow-100">
-      <div className>
+      <div className="text-4xl w-full font-bold mb-4 flex">
         <h1>예산 계산기</h1>
+      </div>
+
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div>
+          <div>지출 항목</div> <div>비용</div>
+          <button>제출</button>
+        </div>
+        <div>
+          <div>리스트</div>
+          <button>목록 지우기</button>
+        </div>
+      </div>
+
+      <div className="w-full font-bold mb-4 flex">
+        <h1>총지출: {}원</h1>
       </div>
     </div>
   );
