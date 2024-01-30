6557fb4c8ad3b57ae9d7a3c1f4f40eb409f5f50f
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-30 16:23:00 +0900

    feat: Implement rendering isEditing is true print editing.. in List.js file

--- src/components/List.js
+++ src/components/List.js
@@ -15,20 +15,24 @@ const List = React.memo(({ id, title, completed, todoData, setTodoData, provided
         setTodoData(newTodoData);
     }
 
-    return (
-        <div 
-        className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounderd`} 
-        key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
-            <div className="items-center">
-                <input type="checkbox" onChange={() => handleCompleChange(id)} defaultChecked={false} />{" "}
-                <span className={completed ? "line-through" : undefined}>{title}</span>
+    if (isEditing) {
+        return ( <div>editing..</div> );
+    } else {
+        return (
+            <div 
+            className={`${snapshot.isDragging ? "bg-gray-400" : "bg-gray-100"} flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 border rounderd`} 
+            key={id} {...provided.draggableProps} ref={provided.innerRef} {...provided.dragHandleProps}>
+                <div className="items-center">
+                    <input type="checkbox" onChange={() => handleCompleChange(id)} defaultChecked={false} />{" "}
+                    <span className={completed ? "line-through" : undefined}>{title}</span>
+                </div>
+                <div className="items-center">
+                    <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x</button>
+                    <button className="px-4 py-2 float-right" onClick={() => setIsEditing(true)}>edit</button>
+                </div>
             </div>
-            <div className="items-center">
-                <button className="px-4 py-2 float-right" onClick={() => handleClick(id)}>x</button>
-                <button className="px-4 py-2 float-right" onClick={() => setIsEditing(true)}>edit</button>
-            </div>
-        </div>
-      );
+        );
+    }
 });
 
 export default List
