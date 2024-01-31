a4bcc4dcf77c9248964f32833d7a57661cd78132
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-31 11:50:49 +0900

    fix: Fix incorrect use of <label for=FORM_ELEMENT> in ExpenseForm.js

--- budget-calculator-app/src/components/ExpenseForm.js
+++ budget-calculator-app/src/components/ExpenseForm.js
@@ -12,11 +12,11 @@ export default function Form({ handleSubmit, expense, setExpense }) {
                 <div className="flex">
                     <div className="py-4 px-3 flex-1">
                         <label className="block text-orange-400" htmlFor="charge">지출 항목</label>
-                        <input name="category" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="text" placeholder="예) 렌트비" value={expense.category} onChange={handleChange} />
+                        <input id="charge" name="category" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="text" placeholder="예) 렌트비" value={expense.category} onChange={handleChange} />
                     </div>
                     <div className="py-4 px-3 flex-1">
                         <label className="block text-orange-400" htmlFor="amount">비용</label>
-                        <input name="amount" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="number" placeholder="예) 100" value={expense.amount} onChange={handleChange} />
+                        <input id="amount" name="amount" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="number" placeholder="예) 100" value={expense.amount} onChange={handleChange} />
                     </div>
                 </div>
                 <button className="px-4 py-2 border rounded" type="submit">제출</button>
