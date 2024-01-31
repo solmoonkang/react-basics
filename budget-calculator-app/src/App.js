91f519fd18b551f4ce1f023a189c59b9449b444c
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-31 10:37:32 +0900

    feat: Implement HTML structure and tailwind CSS in App

--- budget-calculator-app/src/App.js
+++ budget-calculator-app/src/App.js
@@ -1,28 +1,28 @@
-import './App.css';
+import "./App.css";
+import React, { useState, useCallback } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
 function App() {
+
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-yellow-100">
-      <div className="text-4xl w-full font-bold mb-4 flex">
-        <h1>예산 계산기</h1>
-      </div>
-
-      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
-        <div>
-          <div>지출 항목</div> <div>비용</div>
-          <button>제출</button>
+      <div className="w-full p-6 m-4 rounded lg:w-3/4 lg:max-lg">
+        <div className="flex justify-between mb-3 text-4xl">
+          <h1>예산 계산기</h1>
         </div>
-        <div>
-          <div>리스트</div>
-          <button>목록 지우기</button>
+        <ExpenseForm />
+        <ExpenseList />
+        <div className="flex justify-end mt-4">
+          <p className="text-3xl">
+            총지출: 
+            <span>원</span>
+          </p>
         </div>
-      </div>
-
-      <div className="w-full font-bold mb-4 flex">
-        <h1>총지출: {}원</h1>
       </div>
     </div>
-  );
+  )
 }
 
 export default App;
