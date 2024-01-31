09310ec6ccc8050c3a3a3f18fa52ab42a3f4bbb2
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-31 11:52:04 +0900

    feat: Implement handleEnd and drop and drap function in ExpenseForm.js

--- budget-calculator-app/src/components/ExpenseList.js
+++ budget-calculator-app/src/components/ExpenseList.js
@@ -4,13 +4,35 @@ import { Droppable } from 'react-beautiful-dnd';
 import { DragDropContext } from 'react-beautiful-dnd';
 import ExpenseItem from "./ExpenseItem";
 
-const ExpenseList = React.memo(() => {
+const ExpenseList = React.memo(({ handleClick, budgetData, setBudgetData }) => {
+
+    const handleEnd = (result) => {
+        if (!result.destination) return;
+        const newBudgetData = budgetData;
+        const [reorderedItem] = newBudgetData.splice(result.source.index, 1);
+
+        newBudgetData.splice(result.destination.index, 0, reorderedItem);
+        setBudgetData(newBudgetData);
+    }
 
     return (
         <div className="w-full bg-white p-4">
-            <ul className="list-none border-0 p-0">
-                <ExpenseItem />
-            </ul>
+            <DragDropContext onDragEnd={handleEnd}>
+                <Droppable droppableId="budget-calculator">
+                    {(provided, snapshot) => (
+                        <div className="list-none border-0 p-0" {...provided.droppableProps} ref={provided.innerRef}>
+                        {budgetData.map((data, index) => (
+                            <Draggable key={data.id} draggableId={data.id.toString()} index={index}>
+                                {(provided, snapshot) => (
+                                    <ExpenseItem handleClick={handleClick} key={data.id} id={data.id} expense={data.expense} budgetData={budgetData} setBudgetData={setBudgetData} provided={provided} snapshot={snapshot} />
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                        </div>
+                    )}
+                </Droppable>
+            </DragDropContext>
             <button className="px-4 py-2 border rounded">목록 지우기</button>
         </div>
     )
