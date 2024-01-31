0217d76e0098af4340c558c3b41aa963d5a7a857
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-31 11:23:00 +0900

    feat: Initialization variable declaration and implement handleSubmit fuction and integration with ExpenseForm.js

--- budget-calculator-app/src/App.js
+++ budget-calculator-app/src/App.js
@@ -5,6 +5,24 @@ import ExpenseList from "./components/ExpenseList";
 
 function App() {
 
+  const [budgetData, setBudgetData] = useState([]);
+  const [expense, setExpense] = useState({ category: "", amount: "" });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    let newBudgetData = {
+      id: Date.now(),
+      expense: expense
+    }
+
+    // 원래 있던 경비에 새로운 경비를 추가해준다.
+    setBudgetData(prev => [...prev, newBudgetData]);
+    // TODO: 추가적으로, localStorage에도 값을 저장하는 코드를 구현해야 한다.
+
+    // 입력 폼에 있는 내용을 지워줘야 한다.
+    setExpense({ category: "", amount: "" });
+  }
 
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-yellow-100">
@@ -12,7 +30,7 @@ function App() {
         <div className="flex justify-between mb-3 text-4xl">
           <h1>예산 계산기</h1>
         </div>
-        <ExpenseForm />
+        <ExpenseForm handleSubmit={handleSubmit} expense={expense} setExpense={setExpense} />
         <ExpenseList />
         <div className="flex justify-end mt-4">
           <p className="text-3xl">
