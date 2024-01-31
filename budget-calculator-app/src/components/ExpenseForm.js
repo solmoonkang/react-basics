bae48b429dcd6eb5ea9d41352b3df19f315d4fa8
SOLMOON KANG <109902582+solmoonkang@users.noreply.github.com>
2024-01-31 11:23:18 +0900

    feat: Implement handleChange function in ExpenseForm.js

--- budget-calculator-app/src/components/ExpenseForm.js
+++ budget-calculator-app/src/components/ExpenseForm.js
@@ -1,19 +1,22 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({ handleSubmit, expense, setExpense }) {
+
+    const handleChange = (event) => {
+        setExpense({ ...expense, [event.target.name]: event.target.value });
+    }
 
     return (
         <div className="w-full bg-white p-4">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="flex">
                     <div className="py-4 px-3 flex-1">
                         <label className="block text-orange-400" htmlFor="charge">지출 항목</label>
-                        <input className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="text" placeholder="예) 렌트비" />
+                        <input name="category" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="text" placeholder="예) 렌트비" value={expense.category} onChange={handleChange} />
                     </div>
-
                     <div className="py-4 px-3 flex-1">
                         <label className="block text-orange-400" htmlFor="amount">비용</label>
-                        <input className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" placeholder="예) 100" type="number" />
+                        <input name="amount" className="form-control w-full h-12 border-b border-gray-600 bg-transparent outline-none" type="number" placeholder="예) 100" value={expense.amount} onChange={handleChange} />
                     </div>
                 </div>
                 <button className="px-4 py-2 border rounded" type="submit">제출</button>
